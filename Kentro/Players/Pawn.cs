473121872e66841b0b07fac4f597ff26f3512303
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2020-01-29 17:06:30 +0600

    Player Added

---
+++ Kentro/Players/Pawn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentro.Players
+{
+    public class Pawn
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int move { get; set; }
+
+        public Pawn()
+        {
+            this.x = 0;
+            this.y = 0;
+            move = 1;
+        }
+
+
+
+    }
+}
